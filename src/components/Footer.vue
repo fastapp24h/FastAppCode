70d3df264072dc4fee8f240f1aec9cd2f7ed8b81
fastapp24h <FASTAPP24H@GMAIL.COM>
2025-04-06 23:40:08 +0200

    "Aggiunto src/components/Footer.vue"

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer>
+    <p>&copy; {{ currentYear }} Your Company. All rights reserved.</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  computed: {
+    currentYear() {
+      return new Date().getFullYear();
+    },
+  },
+}
+</script>
+
+<style scoped>
+footer {
+  background-color: #333;
+  color: #fff;
+  padding: 16px;
+  text-align: center;
+}
+</style>
